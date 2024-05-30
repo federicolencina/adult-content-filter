3eabc0695bbaca0dcfee755888b06909fa786c30
Fede <166924760+federicolencina@users.noreply.github.com>
2024-05-29 23:45:07 -0300

    Add: Layout for Windows
    Layout.form and Layout files are for Windows only.
    
    These files displays properly only in Windows. And "default" Layout &,
    Layout.form files displays properly only in Linux; (Uses GTK Swing L&F).
    
    If you're testing this app on windows, please move or copy and paste Layout.form & Layout to <main/java/com/fls/>.

---
+++ src/winLayout/Layout.java
@@ -0,0 +1,103 @@
+// #=========================================================================#
+//  #               This file is part of Adult Content Filter               #
+//  #        https://github.com/federicolencina/adult-content-filter        #
+//  #                                                                       #
+//  #                      GNU General Public License                       #
+//  #                                                                       #
+//  #     This program is free software; you can redistribute it and/or     #
+//  #    modify it under the terms of the GNU General Public License as     #
+//  #    published by the Free Software Foundation; either version 3 of     #
+//  #          the License, or at your option any later version.            #
+//  #                                                                       #
+//  #  This program is distributed in the hope that it will be useful, but  #
+//  #       WITHOUT ANY WARRANTY; without even the implied warranty of      #
+//  #          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.         #
+//  #          See the GNU General Public License for more details.         #
+//  #                                                                       #
+//  #        You should have received a copy of the GNU General Public      #
+//  #              License along with this program. If not, see             #
+//  #                    <https://www.gnu.org/licenses/>.                   #
+//  #                                                                       #
+//  #                    Copyright 2024 Federico Lencina                    #
+// #=========================================================================#
+
+package com.fls;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class Layout {
+    private JPanel Layout;
+    private JLabel titleLabel;
+    private JCheckBox primaryCheckBox;
+    private JCheckBox defaultCheckBox;
+    private JButton applySettingsButton;
+    private JLabel osLabel;
+    private JProgressBar progressBar;
+
+    Main main = new Main();
+
+    public Layout() {
+        osLabel.setText(System.getProperty("os.name").toUpperCase());
+        osLabel.setText(System.getProperty("user.dir").toUpperCase());
+        primaryCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (primaryCheckBox.isSelected() && defaultCheckBox.isSelected()) {
+                    defaultCheckBox.setSelected(false);
+                }
+                applySettingsButton.setEnabled(primaryCheckBox.isSelected() || defaultCheckBox.isSelected());
+            }
+        });
+        defaultCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (defaultCheckBox.isSelected() && primaryCheckBox.isSelected()) {
+                    primaryCheckBox.setSelected(false);
+                }
+                applySettingsButton.setEnabled(primaryCheckBox.isSelected() || defaultCheckBox.isSelected());
+            }
+        });
+        applySettingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (primaryCheckBox.isSelected()) {
+                    main.onPrimaryCheckBox(true);
+                } else {
+                    if (defaultCheckBox.isSelected()) {
+                        main.onDefaultCheckbox(true);
+                    }
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        boolean isLinux = System.getProperty("os.name").toLowerCase().startsWith("linux");
+        if (isLinux) {
+            try {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+            } catch (ClassCastException | ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (ClassCastException | ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
+        }
+        JFrame frame = new JFrame("Layout");
+        frame.setContentPane(new Layout().Layout);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(400, 360);
+        frame.setTitle("Adult Content Filter");
+    }
+}
---
+++ src/winLayout/README.txt
@@ -0,0 +1,6 @@
+Layout.form and Layout files are for Windows only.
+
+These files displays properly only in Windows. And "default" Layout &,
+Layout.form files displays properly only in Linux; (Uses GTK Swing L&F).
+
+If you're testing this app on windows, please move or copy and paste Layout.form & Layout to <main/java/com/fls/>.
